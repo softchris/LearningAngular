42676832d39e46b7f03da44eb48936be0033c278
Christoffer Noring <noringc@gmail.com>
2017-12-31 13:41:04 +0000

    Revert "adding Chapter3"
    This reverts commit 1aa8873e4162aa3c8cb9e36a23a0d0bcc7447d49.

--- Chapter3/let.ts
+++
@@ -1,8 +0,0 @@
-let i =3;
-let array = ['one', 'two', 'three'];
-
-for (let i = 0; i <arr.length; i++) {
-  console.log('for-loop i',i);
-}
-
-console.log('outer i', i);
--- Chapter3/types.ts
+++
@@ -1,2 +0,0 @@
-var brand: string = 'Chevrolet';
-var message =`Today it's a happy day! I just bought a new ${brand} car`;
